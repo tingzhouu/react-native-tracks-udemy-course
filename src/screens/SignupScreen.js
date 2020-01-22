a5df9280fc42beef8ab34abe9f2b298365e40144
tingzhouu <limtingzhou@gmail.com>
2020-01-23 00:51:07 +0800

    add react-native-elements and styling

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, StyleSheet, } from 'react-native'
+import { Text, Input, Button} from 'react-native-elements';
+
+import Spacer from '../components/Spacer';
 
 const SignUpScreen = ({navigation}) => {
   return (
     <View>
-      <Text>SignUpScreen</Text>
-      <Button
-        title="Go to Signin"
-        onPress={() => navigation.navigate('Signin')}
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input
+        label="Email"
       />
-      <Button
-        title="Go to Main Flow"
-        onPress={() => navigation.navigate('mainFlow')}
+      <Spacer />
+      <Input
+        label="Password"
       />
+      <Spacer>
+        <Button
+          title="Sign Up"
+        />
+      </Spacer>
     </View>
   )
 }
