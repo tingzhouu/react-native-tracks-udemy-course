e5017ff43f1f94170eb22069dbbd0c253612d605
tingzhouu <limtingzhou@gmail.com>
2020-02-14 13:38:06 +0800

    add state and adjust styling for signup screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, StyleSheet, } from 'react-native'
 import { Text, Input, Button} from 'react-native-elements';
 
 import Spacer from '../components/Spacer';
 
 const SignUpScreen = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
       <Input
         label="Email"
+        onChangeText={setEmail}
       />
       <Spacer />
       <Input
+        secureTextEntry
         label="Password"
+        onChangeText={setPassword}
       />
       <Spacer>
         <Button
@@ -26,8 +32,18 @@ const SignUpScreen = ({navigation}) => {
   )
 }
 
-const styles = StyleSheet.create({
+SignUpScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerShown: false,
+  }
+}
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200,
+  }
 })
 
 
