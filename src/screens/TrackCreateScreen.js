77acb8a7fe2567e9713c91b022fa20c0030eeafd
tingzhouu <limtingzhou@gmail.com>
2020-02-16 21:40:50 +0800

    add track form update locations array

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -5,17 +5,21 @@ import { SafeAreaView, withNavigationFocus } from "react-navigation";
 
 import "../_mockLocation";
 import Map from "../components/Map";
+import TrackForm from '../components/TrackForm';
 import { Context as LocationContext } from "../context/LocationContext";
 import useLocation from "../hooks/useLocation";
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { addLocation } = useContext(LocationContext);
-  const [err] = useLocation(isFocused, addLocation);
+  const { state, addLocation } = useContext(LocationContext);
+  const [err] = useLocation(isFocused, (location) => {
+    addLocation(location, state.recording);
+  });
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2>Create a Track</Text>
       {err ? <Text>{err}</Text> : null}
       <Map />
+      <TrackForm />
     </SafeAreaView>
   );
 };
